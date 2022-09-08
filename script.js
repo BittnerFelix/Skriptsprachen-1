2bb7067437e4df0badf65aace7f49f5cdb836c90
Felix <94891956+BittnerFelix@users.noreply.github.com>
2022-09-08 10:31:19 +0000

    Mad Lips Test

---
+++ script.js
@@ -0,0 +1,20 @@
+let Adjektiv1 = prompt("Ein Adjektiv");
+let Nationalität = prompt("Eine Nationalität");
+let Name1 = prompt("ein Name");
+let Nomen1 = prompt("ein Nomen");
+let adjektiv2 = prompt("ein adjektiv");
+let adjektiv3 = prompt("ein adjektiv");
+let adjektiv4 = prompt("ein adjektiv");
+let mehrzahl_Nomen = prompt("ein Nomen in der Mehrzahl");
+let endung1 = prompt("eine Endung");
+let endung2 = prompt("eine Endung");
+let nomen2 = prompt("ein Nomen");
+let Essen1 = prompt("ein Essen");
+let Essen2 = prompt("ein Essen");
+let zahl1 = prompt("Eine Zahl");
+
+console.log("Pizza wurde von einem "+ Adjektiv1+" "+ Nationalität+" Koch erfunden der "+ Name1+" hieß."+ "Um Pizza zu machen, fängt man einen Klumpen ein "+ Nomen1+" an und rollt ihn in dünnen, runden, "+adjektiv2+" Scheiben. Darauf verteilt man "+adjektiv3+" Sauce sowie "+adjektiv4+" Käse und frisch geschnitten "+mehrzahl_Nomen+". Danach muss man die Pizza in ein"+endung1+" sehr heiß"+endung2+" " +nomen2+" backen. Manchen Kindern schmeckt "+Essen1+" Pizza am besten, aber meine Lieblingspizza ist"+Essen2+"-Pizza. Wenn ich könnte, würde ich "+zahl1+" Mal am Tag Pizza essen.");
+
+
+
+
